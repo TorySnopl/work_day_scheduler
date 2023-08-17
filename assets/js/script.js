67abc373a6aa29876bc4da12b67177673f3ca385
TorySnopl <Paramedic064@gmail.com>
2023-08-16 19:26:07 -0700

    all functions working except localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,83 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
+
+// dayJs used to set current date at top
+let dayJsObject = dayjs();
+let dateEl = document.querySelector('#currentDay');
+dateEl.textContent = dayJsObject.format("MM/D/YYYY hh:mm A");
+
+let saveBtn = document.querySelectorAll('.saveBtn');
+let inputField = '';
+
+
+
+
+
+
+
+// function to update classes on the calendar every minute
+  function updateBlockClasses() {
+    
+    var currentHour = dayjs().hour();
+
+    localStorage.getItem('input');
+
+    
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id'));
+     
+      $(this).removeClass('past present future');
+
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+        $(this).removeClass('present');
+        $(this).removeClass('future');
+      } else if (blockHour === currentHour) {
+        $(this).addClass('present');
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+      } else {
+        $(this).addClass('future');
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+      }
+    });
+  }
+
+  updateBlockClasses();
+  setInterval(updateBlockClasses, 60000);
+  
+
+  // function to iterate over button node list and save input
+
+ 
+
+  
+
+  saveBtn.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      
+      inputField = btn.previousElementSibling;
+
+      let task = inputField.value;
+      
+      alert('Task Saved: ' + task);
+
+      localStorage.setItem('input', task);
+      
+      
+    
+    });
   });
+
+
+
+
+  
+});
+ 
+
+
+
+    
